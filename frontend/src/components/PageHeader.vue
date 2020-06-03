1bc451cbcfa8f85a486df9327a6e92706d7f2db4
Claudio Gomes <cvgomes@gmail.com>
2020-06-03 15:46:30 +0200

    Minor changes: VSCode formatting and using class instead of id

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -1,39 +1,39 @@
 <template>
-  <header id="page-header-cmp">
-      <h1>Tasks</h1>
-      <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </nav>
+  <header class="page-header">
+    <h1>Tasks</h1>
+    <nav>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'PageHeader',
+  name: "PageHeader"
 };
 </script>
 
 <style scoped>
-    header {
-        background: #333;
-        color: #fff;
-        text-align: center;
-        padding: 10px;
-    }
-    header h1 {
-        margin-top: 8px;
-        margin-bottom: 8px;
-    }
-    header a {
-        color: #fff;
-        padding: 3px;
-    }
-    nav a {
-        color: #ccc;  
-    }
-    nav a.router-link-exact-active {
-        font-weight: bold;
-        color: #fff;  
-    }
+header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+header h1 {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+header a {
+  color: #fff;
+  padding: 3px;
+}
+nav a {
+  color: #ccc;
+}
+nav a.router-link-exact-active {
+  font-weight: bold;
+  color: #fff;
+}
 </style>
